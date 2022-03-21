b2f67d31f1f83c5e51822f71ed9a608abda769ab
Alexandra Davis <alex.davis806@gmail.com>
2022-03-20 18:46:11 -0700

    commit tree 127eaf27bc16b39c45586cfa2369fe78be33a10e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textId = document.getElementById('text');
+    textId.textContent = 'This is really cool!';
+  });
+
